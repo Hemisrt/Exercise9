c1b9a3a3d6401dc74ed23f300f5f7b9f44bbe6d1
trentonjavadroid1660 <59889828+trentonjavadroid1660@users.noreply.github.com>
2020-03-10 13:33:55 -0400

    Update Main.java

--- src/com/company/Greenlee/Main.java
+++ src/com/company/Greenlee/Main.java
@@ -6,8 +6,8 @@ public class Main {
     // Exercise 9
     // CSCI 1660 - Android Programming Fundamentals (Java)
     public static void main(String[] args) {
-        Email email = new Email("Trenton Greenlee", "trentongreenlee@gmail.com");
-        Phone phone = new Phone("Trenton Greenlee", "614-981-6680");
+        Email email = new Email("Naruto", "nuzumaki@gmail.com");
+        Phone phone = new Phone("Naruto", "614-342-6544");
 
         email.contact();
         phone.contact();
